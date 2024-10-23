0389f25400132c8bb4897f24ab0e74ee8d86894e
Iftikhar Khan <ikhan@oas.de>
2024-10-23 22:13:34 +0200

    DbContext, EF Core Migrations, Create Controllers and Actions

---
+++ API/CodePlus.API/CodePlus.API/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using CodePlus.API.Data;
+using CodePlus.API.Models.Domain;
+using CodePlus.API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePlus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDBContext dBContext;
+
+        public CategoriesController(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dBContext.Categories.AddAsync(category);
+            await dBContext.SaveChangesAsync();
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ API/CodePlus.API/CodePlus.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CodePlus.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePlus.API.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
